37642c360a629cc116b175c25d6d86f4c8d9a69a
Deathknight101 <120172437+Deathknight101@users.noreply.github.com>
2023-09-23 18:17:35 +0600

    Adding Favourites

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/screens/catagories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -20,6 +20,6 @@ class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const CategoriesScreen());
+    return MaterialApp(theme: theme, home: const TabsScreen());
   }
 }
